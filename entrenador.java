e664a376073fedf15c28faf595d595684165b46a
Fatake Raicos <pace_lozano@hotmail.com>
2020-04-03 13:40:15 -0600

    Gerando fichero y veces de experimentos

--- entrenador.java
+++ entrenador.java
@@ -21,42 +21,46 @@ public class Entrenador{
         this.kfolds = kfolds;
     }
 
-    public String entrenar(){
-        String resultados = null;
-        try{
-            FileReader trainreader = new FileReader("ForestFireNormalizado.arff");
-            FileReader testreader = new FileReader("ForestFireNormalizadoTest.arff");
+    //Cuantas veces se va a entrenar
+    public String experimentos(int numeroExperimentos){
+        String resultados = "";
 
-            Instances train = new Instances(trainreader);
-            Instances test = new Instances(testreader);
-            train.setClassIndex(train.numAttributes() - 1);
-            test.setClassIndex(test.numAttributes() - 1);
-
-            MultilayerPerceptron mlp = new MultilayerPerceptron();
-            //mlp.setOptions(Utils.splitOptions("-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H 4"));
-            
-            //Setting Parameters
-            mlp.setHiddenLayers( neuronasCapas );//Neuronas, Capas
-            mlp.setTrainingTime( epocas );//Epocas
-            mlp.setLearningRate( learningRate );//LearningRate
-            mlp.setMomentum( momentum );//Momentum
-
-            //Se pone a entrenar
-            mlp.buildClassifier(train);
-
-            //Evalua resultados
-            
-            Evaluation eval = new Evaluation(train);
-
-            eval.crossValidateModel(mlp, train, kfolds, new Random(1));
-
-            System.out.println(eval.errorRate()); //Error cuadrático Medio
-            //System.out.println(eval.toSummaryString("\nResultados\n======\n", false));
-            trainreader.close();
-            testreader.close();
-            resultados = ""+eval.toSummaryString("\nResultados\n======\n", false);
-        } catch(Exception ex){
-            ex.printStackTrace();
+        for (int i = 0; i < numeroExperimentos; i++) {
+            try{
+                FileReader trainreader = new FileReader("ForestFireNormalizado.arff");
+                FileReader testreader = new FileReader("ForestFireNormalizadoTest.arff");
+    
+                Instances train = new Instances(trainreader);
+                Instances test = new Instances(testreader);
+                train.setClassIndex(train.numAttributes() - 1);
+                test.setClassIndex(test.numAttributes() - 1);
+    
+                MultilayerPerceptron mlp = new MultilayerPerceptron();
+                //mlp.setOptions(Utils.splitOptions("-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H 4"));
+                
+                //Setting Parameters
+                mlp.setHiddenLayers( neuronasCapas );//Neuronas, Capas
+                mlp.setTrainingTime( epocas );//Epocas
+                mlp.setLearningRate( learningRate );//LearningRate
+                mlp.setMomentum( momentum );//Momentum
+    
+                //Se pone a entrenar
+                mlp.buildClassifier(train);
+    
+                //Evalua resultados
+                
+                Evaluation eval = new Evaluation(train);
+    
+                eval.crossValidateModel(mlp, train, kfolds, new Random(1));
+    
+                System.out.println(eval.errorRate()); //Error cuadrático Medio
+                //System.out.println(eval.toSummaryString("\nResultados\n======\n", false));
+                trainreader.close();
+                testreader.close();
+                resultados += ""+eval.toSummaryString("Resultados"+(i+1)+"\n======\n", false)+"\n\n";
+            } catch(Exception ex){
+                ex.printStackTrace();
+            }
         }
 
         //regresa los resultados
@@ -65,14 +69,31 @@ public class Entrenador{
 
     //Main
     public static void main(String args[]){
+        FileWriter fichero = null;
         String neuronasCapas = "3,3";
         int epocas = 200;
-        Float learningRate = 0.3;
-        Float momentum = 0.2;
+        Float learningRate = new Float(0.3);
+        Float momentum = new Float(0.2);
         int kfolds = 5;//Nume ro de validaciones Cruzadas
         
         Entrenador training = new Entrenador(neuronasCapas,epocas,learningRate,momentum,kfolds);
-        System.out.println(training.entrenar());
+
+        try{
+            fichero = new FileWriter("resultados.txt",true);
+            PrintWriter pw  = new PrintWriter(fichero);
+            pw.print("N/C:"+neuronasCapas+" E:"+epocas+" LR:"+learningRate+" M:"+momentum+" KF:"+kfolds+" ");
+            pw.println( training.experimentos(3) );
+            pw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fichero)
+                    fichero.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
     }
 
 }
