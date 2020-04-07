be7d20593a47955141ff924f3daea85e856eab94
Fatake Raicos <pace_lozano@hotmail.com>
2020-04-07 17:10:26 -0500

    Entrenador calcula Capas Y epocas| gitignore cambios

--- compilar.bat
+++ compilar.bat
@@ -2,8 +2,7 @@
 ECHO Compilando...
 javac -cp ".;weka.jar" Entrenador.java
 
-CLS
 ECHO ejecutando
-java --add-opens=java.base/java.lang=ALL-UNNAMED -cp ".;weka.jar" Entrenador
+java --add-opens=java.base/java.lang=ALL-UNNAMED -cp ".;weka.jar" Entrenador > epocasEro.cvs
 pause
 exit
--- entrenador.java
+++ entrenador.java
@@ -43,7 +43,7 @@ public class Entrenador{
             //System.out.println(eval.toSummaryString("\nResultados\n======\n", false));
             trainreader.close();
             testreader.close();
-            resultados += ""+eval.toSummaryString("Resultados\n<------------------------->\n", false)+"\n\n";
+            resultados += "\n"+eval.toSummaryString("Resultados\n<------------------------->\n", false)+"\n\n";
         } catch(Exception ex){
             ex.printStackTrace();
         }
@@ -52,13 +52,35 @@ public class Entrenador{
         return resultados;
     }
 
+    public static String capas(int numeroCapas, int neuronas){
+        String buffer = "";
+        for (int i = 1; i <= numeroCapas; i++) {
+            if (i == 1) {
+                buffer += neuronas;
+            }else{
+                buffer += ","+neuronas;
+            }
+        }
+        System.out.print(buffer+" ");
+        return buffer;
+    }
     //Main
     public static void main(String args[]){
+        int mejoresNeuronas[] = {16,25,21,24,26,17,27,22,14,15};
+
+        //Global es mejor 3 capas
+        String mejoresCapas[] = {"16","25","27,27","17,17","27,27,27",
+                                    "14,14,14","21,21,21,21",
+                                    "27,27,27,27",
+                                    "16,16,16,16,16",
+                                    "25,25,25,25,25"};
         FileWriter fichero = null;
         try{
             fichero = new FileWriter("resultados.txt",true);
             PrintWriter pw  = new PrintWriter(fichero);
             
+            //Calculo de Neuronas
+            /*
             int min = 3, max = 27;
             for (int i = min; i <= max; i++) {
                 String buffer = "";
@@ -74,6 +96,49 @@ public class Entrenador{
             
                 //Imprime buffer
                 pw.println( buffer );
+            }
+            */
+           
+            //Calculo de Capas
+            /*
+            int min = 1, max = 5;
+            for (int neurona : mejoresNeuronas) {
+                for (int i = min; i <= max; i++) {
+                    String buffer = "";
+                    //+","+i
+                    String neuronasCapas = capas(i, neurona);
+                    int epocas = 200;
+                    Float learningRate = new Float(0.3);
+                    Float momentum = new Float(0.2);
+                    int kfolds = 5;//Numero de Validaciones Cruzadas
+                    
+                    buffer += "Neuronas y Capas:"+neuronasCapas+" Epocas:"+epocas+" LR:"+learningRate+" M:"+momentum+" KF:"+kfolds+" ";
+                    buffer += entrenar(neuronasCapas, epocas, learningRate, momentum, kfolds);
+                
+                    //Imprime buffer
+                    pw.println( buffer );
+                }
+            }
+            */
+
+            //Calculo de Epocas
+            System.out.print("Epocas,Capas,ErrorCuadratico");
+            int min = 150, max = 2500;
+            for (String capa : mejoresCapas) {
+                for (int i = min; i <= max; i=i+25) {
+                    String buffer = "";
+                    String neuronasCapas = capa;
+                    int epocas = i;
+                    Float learningRate = new Float(0.3);
+                    Float momentum = new Float(0.2);
+                    int kfolds = 5;//Numero de Validaciones Cruzadas
+                    System.out.print(""+i+","+capa+",");
+                    buffer += "Neuronas y Capas:"+neuronasCapas+" Epocas:"+epocas+" LR:"+learningRate+" M:"+momentum+" KF:"+kfolds+" ";
+                    buffer += entrenar(neuronasCapas, epocas, learningRate, momentum, kfolds);
+                
+                    //Imprime buffer
+                    pw.println( buffer );
+                }
             }
             
             //Fin de escritura en archivo
